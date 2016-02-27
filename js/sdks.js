139bf25a5148c4825e67470c014713a96eef2be7
Arturs Sosins <ar2rsawseen@gmail.com>
2016-02-27 23:21:23 +0200

    Add information about Android SDK

--- js/sdks.js
+++ js/sdks.js
@@ -6,5 +6,14 @@ var sdks = {
         download: "https://github.com/Countly/countly-sdk-web/releases",
         docs: "http://resources.count.ly/docs/countly-sdk-for-web",
         description: "Track users and user behavior in your websites or native app webviews"
+    },
+    android: {
+        name: "Android SDK",
+        image: "images/android.png",
+        repo: "https://github.com/Countly/countly-sdk-android",
+        download: "https://github.com/Countly/countly-sdk-android/releases",
+        docs: "http://resources.count.ly/docs/countly-sdk-for-android",
+        description: "Track users and user behavior in your android based apps"
+    },
     }
 };
