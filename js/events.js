2076216295aafb0031d2f736bd99baa350cabb61
Arturs Sosins <ar2rsawseen@gmail.com>
2016-02-27 23:23:05 +0200

    Add event generation for Android

--- js/events.js
+++ js/events.js
@@ -3,5 +3,28 @@ var events = {
         return "<script type='text/javascript'>\n"+
         "Countly.q.push(['add_event',"+JSON.stringify(data, null, 3)+"]);\n"+
         "</scri" + "pt>";
+    },
+    android: function(data){
+        if(!data.sum && !data.segmentation){
+            return 'Countly.sharedInstance().recordEvent("'+data.key+'", '+data.count+');';
+        }
+        else if(!data.segmentation){
+            return 'Countly.sharedInstance().recordEvent("'+data.key+'", '+data.count+', '+data.sum+');';
+        }
+        else if(!data.sum){
+            var ret = "HashMap<String, String> segmentation = new HashMap<String, String>();\n";
+            for(var key in data.segmentation){
+                ret += 'segmentation.put("'+key+'", "'+data.segmentation[key]+'");\n';
+            }
+            return ret + 'Countly.sharedInstance().recordEvent("'+data.key+'", segmentation, '+data.count+');';
+        }
+        else{
+            var ret = "HashMap<String, String> segmentation = new HashMap<String, String>();\n";
+            for(var key in data.segmentation){
+                ret += 'segmentation.put("'+key+'", "'+data.segmentation[key]+'");\n';
+            }
+            return ret + 'Countly.sharedInstance().recordEvent("'+data.key+'", segmentation, '+data.count+', '+data.sum+');';
+        }
+    },
     }
 };
