f30ef6a12e0a723503bd75c7d8bded16c8b92329
Arturs Sosins <ar2rsawseen@gmail.com>
2016-02-28 09:50:17 +0200

    Check if sdk integration provided

--- js/sdks.js
+++ js/sdks.js
@@ -5,7 +5,8 @@ var sdks = {
         repo: "https://github.com/Countly/countly-sdk-web",
         download: "https://github.com/Countly/countly-sdk-web/releases",
         docs: "http://resources.count.ly/docs/countly-sdk-for-web",
-        description: "Track users and user behavior in your websites or native app webviews"
+        description: "Track users and user behavior in your websites or native app webviews",
+        integration: true
     },
     android: {
         name: "Android SDK",
