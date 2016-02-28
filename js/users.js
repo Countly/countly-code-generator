1acd53dc9cbe8458af1fbe1dce7c0694bcfdf8d0
Arturs Sosins <ar2rsawseen@gmail.com>
2016-02-28 22:50:40 +0200

    Add ios user generation

--- js/users.js
+++ js/users.js
@@ -20,5 +20,35 @@ var users = {
         script+
         "});\n"+
         "</scri" + "pt>";
+    },
+    ios: function(data){
+        var script = "";
+        for(var i in data){
+            if(i != "custom"){
+                if(i == "byear")
+                    script += "CountlyUserDetails.sharedInstance.birthYear = "+data[i]+";\n";
+                else
+                    script += "CountlyUserDetails.sharedInstance."+i+" = @\""+data[i]+"\";\n";
+            }
+        }
+        if(data.custom){
+            var map = {
+                set: "set",
+                set_once: "setOnce",
+                increment: "increment",
+                multiply: "multiply",
+                max: "max",
+                min: "min",
+                push_unique: "pushUnique",
+                pull: "pull",
+            };
+            for(var i in data.custom){
+                if(data.custom[i].action == "increment" || data.custom[i].action == "multiply" || data.custom[i].action == "min" || data.custom[i].action == "max")
+                    script += "[CountlyUserDetails.sharedInstance "+map[data.custom[i].action]+":@\""+i+"\" value:"+data.custom[i].value+"];\n";
+                else
+                    script += "[CountlyUserDetails.sharedInstance "+map[data.custom[i].action]+":@\""+i+"\" value:@\""+data.custom[i].value+"\"];\n";
+            }
+        }
+        return script;
     }
 };
