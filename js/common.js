05465c4f2da7321dbcccd14f777b048f537c04ca
Arturs Sosins <ar2rsawseen@gmail.com>
2016-03-07 15:37:50 +0200

    Track usage

--- js/common.js
+++ js/common.js
@@ -10,3 +10,28 @@ function parseParams(url){
    }
    return params;
 }
+
+//some default pre init
+var Countly = Countly || {};
+Countly.q = Countly.q || [];
+
+//provide countly initialization parameters
+Countly.app_key = 'e476ab9e70b1e5c1da60e9da7f2b7d99c24dcff7';
+Countly.url = 'https://stats.count.ly'; //or none for default countly cloud
+
+Countly.q.push(['track_sessions']);
+Countly.q.push(['track_pageview']);
+Countly.q.push(['track_clicks']);
+Countly.q.push(['track_errors']);
+Countly.q.push(['track_links']);
+Countly.q.push(['track_forms']);
+
+//load countly script asynchronously
+(function() {
+   var cly = document.createElement('script'); cly.type = 'text/javascript';
+   cly.async = true;
+   //enter url of script here
+   cly.src = 'https://stats.count.ly/sdk/web/countly.min.js';
+   cly.onload = function(){Countly.init()};
+   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(cly, s);
+})();
