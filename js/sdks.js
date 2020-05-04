111c1fb089ff7679213d848863f3fe76b4bdbb5e
Arturs Sosins <ar2rsawseen@gmail.com>
2020-05-04 20:49:03 +0300

    [versions] bump

--- js/sdks.js
+++ js/sdks.js
@@ -6,7 +6,7 @@ var sdks = {
         download: "https://github.com/Countly/countly-sdk-ios/releases",
         docs: "http://resources.count.ly/docs/countly-sdk-for-ios-and-os-x",
         description: "Track users and user behavior in your iOS, watchOS, tvOS and MacOS apps",
-        version: "18.08",
+        version: "20.04",
         integration: true
     },
     android: {
@@ -16,7 +16,7 @@ var sdks = {
         download: "https://github.com/Countly/countly-sdk-android/releases",
         docs: "http://resources.count.ly/docs/countly-sdk-for-android",
         description: "Track users and user behavior in your android based apps",
-        version: "18.08.1",
+        version: "20.04",
         integration: true
     },
     web: {
@@ -26,7 +26,7 @@ var sdks = {
         download: "https://github.com/Countly/countly-sdk-web/releases",
         docs: "http://resources.count.ly/docs/countly-sdk-for-web",
         description: "Track users and user behavior in your websites or native app webviews",
-        version: "18.8.2",
+        version: "20.4.0",
         integration: true
     },
     windows: {
@@ -36,7 +36,7 @@ var sdks = {
         download: "https://github.com/Countly/countly-sdk-windows-phone/releases",
         docs: "http://resources.count.ly/docs/countly-sdk-for-windows-phone",
         description: "Track users and user behavior in your Windows Mobile, Windows Desktop or .NET app",
-        version: "16.12"
+        version: "19.08.1"
     },
     nodejs: {
         name: "NodeJS SDK",
@@ -45,7 +45,7 @@ var sdks = {
         download: "https://github.com/Countly/countly-sdk-nodejs/releases",
         docs: "http://resources.count.ly/docs/countly-sdk-for-nodejs",
         description: "Track users and user behavior on platforms with nodejs environment",
-        version: "18.8.1",
+        version: "20.04",
         integration: true
     }
 };
